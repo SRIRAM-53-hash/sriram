630c57de37ea803f8cb6a3ee22d8ac29b2788528
SRIRAM-53-hash <sriramsivakumar2005@gmail.com>
2024-10-07 09:28:15 +0530

    tech updated

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -49,6 +49,9 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
     </Row>
   );
 }
