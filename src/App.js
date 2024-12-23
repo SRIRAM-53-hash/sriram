7d75c9537cba241c4a5451bdef0b67644a099c12
SRIRAM-53-hash <sriramsivakumar2005@gmail.com>
2024-12-23 12:37:31 +0530

    removing functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
+import Preloader from "./components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-import AboutCard from "./components/About/AboutCard";
 import Projects from "./components/Projects/Projects";
 import Certificates from "./components/certificates/certifi";
 import Footer from "./components/Footer";
@@ -18,7 +17,6 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import certificates from "./components/certificates/certificard"
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -30,6 +28,7 @@ function App() {
 
     return () => clearTimeout(timer);
   }, []);
+
   return (
     <Router>
       <Preloader load={load} />
@@ -41,11 +40,13 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="/certificates" element={<Certificates/>} />
+          <Route path="/certificates" element={<Certificates />} />
           {/* <Route path="*" element={<Navigate to="/"/>} /> */}
         </Routes>
         <Footer />
       </div>
     </Router>
   );
-}  
+}
+
+export default App; // Ensure App is exported as default
