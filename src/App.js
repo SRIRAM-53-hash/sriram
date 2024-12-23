6633d2af416481b11014c8f5c27c1a812f3c16ce
SRIRAM-53-hash <sriramsivakumar2005@gmail.com>
2024-12-23 12:10:41 +0530

    removing functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Preloader from './components/Pre'; // Ensure this path is correct
+import Preloader from './components/Preloader';
 import Navbar from './components/Navbar';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
@@ -9,32 +9,13 @@ import About from './components/About';
 import Resume from './components/Resume';
 import Certificates from './components/Certificates';
 import Footer from './components/Footer';
-import PopupAd from './components/PopupAd'; // Import PopupAd
 
 function App() {
   const [load, setLoad] = useState(true);
-  const [showAd, setShowAd] = useState(false); // Add state for showAd
 
   useEffect(() => {
-    const preloaderTimer = setTimeout(() => {
-      setLoad(false);
-    }, 1200);
-
-    // Popup ad timeout
-    const hasSeenAd = localStorage.getItem("hasSeenAd");
-    if (!hasSeenAd) {
-      const popupTimer = setTimeout(() => {
-        setShowAd(true);
-        localStorage.setItem("hasSeenAd", "true");
-      }, 3000); // Show ad 3 seconds after load
-
-      return () => {
-        clearTimeout(preloaderTimer);
-        clearTimeout(popupTimer);
-      };
-    }
-
-    return () => clearTimeout(preloaderTimer);
+    const timer = setTimeout(() => setLoad(false), 3000);
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -43,7 +24,6 @@ function App() {
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
-        {showAd && <PopupAd />} {/* Show PopupAd if showAd is true */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
